f621a7bf4dd627d784c5f14abfdf35897114c82d
Victor Teka <ambukateka@gmail.com>
2019-10-09 04:45:11 +0300

    Add log in functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@
 //**************UI LOGIC *********//
 
 $("document").ready(function() {
+  //signup
   $("#signup-form").submit(function(event) {
     event.preventDefault();
     //user's data
     var username = document.getElementById("username").value;
     var email = document.getElementById("signup-email").value;
     var password = document.getElementById("signup-password").value;
-    alert(username);
     var existingUsers = JSON.parse(localStorage.getItem("allEntries"));
     if (existingUsers == null) existingUsers = [];
     var user = {
@@ -20,6 +20,26 @@ $("document").ready(function() {
     localStorage.setItem("user", JSON.stringify(user));
     existingUsers.push(user);
     localStorage.setItem("allEntries", JSON.stringify(existingUsers));
+    this.reset();
+  });
+  //sign in
+  $("#login-form").submit(function(event) {
+    event.preventDefault();
+    var email = document.getElementById("my-email").value;
+    var password = document.getElementById("my-password").value;
+    var usersData = JSON.parse(localStorage.getItem("allEntries"));
+    var emailArray = [];
+    var passwordArray = [];
+    usersData.forEach(function(user) {
+      emailArray.push(user.email);
+      passwordArray.push(user.password);
+    });
+    if (emailArray.includes(email) && passwordArray.includes(password)) {
+      alert("Logged in successfull");
+    } else {
+      alert("Incorrect email or password");
+    }
+
     this.reset();
   });
 });
